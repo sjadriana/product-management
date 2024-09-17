983f6c94feed09e4e1a619ec1b5df92bd7f1ecb6
Adriana Silva <adriana.jesus@luizalabs.com>
2024-09-17 20:12:35 -0300

    fix: testando configuração de deploy

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/produtos-app'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
